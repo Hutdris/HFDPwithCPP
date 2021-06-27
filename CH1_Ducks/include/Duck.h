44163ece46bbf60dd95a8458f999032a29c10ae9
Hutdris <hutdris@hotmail.com>
2021-06-27 21:48:23 +0800

    Implementaion of Duck, but stuck on  unique_ptr convertion.

--- CH1_Ducks/include/Duck.h
+++ CH1_Ducks/include/Duck.h
@@ -4,15 +4,72 @@
 // Cpp has no interface, so use abstract class to apporch
 class QuackBehavior {
 public:
-    virtual ~QuackBehavior() = 0;
+    virtual void quack() = 0;
+    virtual ~QuackBehavior() = default;
 };
+
+class SimpleQuack : QuackBehavior {
+public:
+    SimpleQuack() {};
+    ~SimpleQuack() {};
+    void quack() override;
+};
+class MuteQuack : QuackBehavior {
+public:
+    MuteQuack() {};
+    ~MuteQuack() {};
+    void quack() override;
+};
+class DigiQuack : QuackBehavior {
+public:
+    DigiQuack() {};
+    ~DigiQuack() {};
+    void quack() override;
+};
+
+
 class FlyBehavior {
 public:
-    virtual ~FlyBehavior() = 0;
+    virtual void fly() = 0;
+    virtual ~FlyBehavior() = default;
+};
+class FlyWithWings : FlyBehavior {
+public:
+    FlyWithWings() {};
+    ~FlyWithWings() {};
+    void fly() override;
+};
+class FlyWithRocket : FlyBehavior {
+public:
+    FlyWithRocket() {};
+    ~FlyWithRocket() {};
+    void fly() override;
+};
+class CanNotFly : FlyBehavior {
+public:
+    CanNotFly() {};
+    ~CanNotFly() {};
+    void fly() override;
 };
 
 class Duck {
 public:
+    template<typename T>
+    void setQuack(std::unique_ptr<T> quackInstance);
+
+    template<typename T>
+    void setFly(std::unique_ptr<T> flyInstance);
+    void performQuack();
+    void performFly();
+    virtual ~Duck() = default;
+private:
     std::unique_ptr<QuackBehavior> mQuackInstance;
     std::unique_ptr<FlyBehavior> mFlyInstance;
 };
+
+class WildDuck : Duck {
+public:
+    WildDuck();
+
+};
+
--- CH1_Ducks/src/Duck.cpp
+++ CH1_Ducks/src/Duck.cpp
@@ -1,1 +1,51 @@
 #include <Duck.h>
+
+void SimpleQuack::quack() {
+    std::cout << "Quack, Quack!" << std::endl;
+}
+void MuteQuack::quack() {
+    std::cout << "...?" << std::endl;
+}
+void DigiQuack::quack() {
+    std::cout << "QUACK QUACK!! bee bee." << std::endl;
+}
+
+void FlyWithWings::fly() {
+    std::cout << "Fly with wings!!" << std::endl;
+}
+void FlyWithRocket::fly() {
+    std::cout << "Fly with ROCKEETTT!!!" << std::endl;
+}
+void CanNotFly::fly() {
+    std::cout << "...!" << std::endl;
+}
+
+
+template<typename T>
+void Duck::setQuack(std::unique_ptr<T> quackInstance) {
+    if (quackInstance) {
+        mQuackInstance = std::move(quackInstance);
+    }
+}
+
+template<typename T>
+void Duck::setFly(std::unique_ptr<T> flyInstance) {
+    if (flyInstance) {
+        mFlyInstance = std::move(flyInstance);
+    }
+}
+
+void Duck::performQuack() {
+    if (mQuackInstance) {
+        mQuackInstance->quack();
+    }
+}
+void Duck::performFly() {
+    if (mFlyInstance) {
+        mFlyInstance->fly();
+    }
+}
+
+WildDuck::WildDuck() {
+    setQuack(std::move(std::unique_ptr<SimpleQuack>()));
+}
